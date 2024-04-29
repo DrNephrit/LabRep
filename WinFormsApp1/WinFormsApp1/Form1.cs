d2b1a41e0281933d7e1ca9dd646d503ff1ceb8af
Nephrit <andrylite2006@gmail.com>
2024-04-29 16:24:00 +0300

    готовый код с условиями на исключения

--- WinFormsApp1/WinFormsApp1/Form1.cs
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -22,28 +22,35 @@ namespace WinFormsApp1
 
         private void button1_Click(object sender, EventArgs e) // Добавление заказа
         {
-            ListViewItem item = new ListViewItem(textBox3.Text);
-
-            listView1.Items.Add(item);
-            textBox3.Clear();
-
-            int id = listView1.Items.Count;
-
-            string order = id.ToString();
-
-            item.SubItems.Add(order);
-            item.SubItems.Add("в корзине");
-
-            if (listView1.Items.Count > 0)
+            if (!string.IsNullOrWhiteSpace(textBox3.Text))
             {
-                sum += 200;
+                ListViewItem item = new ListViewItem(textBox3.Text);
+
+                listView1.Items.Add(item);
+
+                int id = listView1.Items.Count;
+
+                string order = id.ToString();
+
+                item.SubItems.Add(order);
+                item.SubItems.Add("в корзине");
+
+                if (listView1.Items.Count > 0)
+                {
+                    sum = sum + 200;
+
+                }
+
+                string sumtext = sum.ToString();
+                if (listView1.Items.Count > 0)
+                    label2.Text = sumtext;
+
+                textBox3.Clear();
             }
-
-            string sumtext = sum.ToString();
-            if (listView1.Items.Count > 0)
-                label2.Text = sumtext;
-
-            textBox3.Clear();
+            else
+            {
+                MessageBox.Show("Заполните поле для добавления заказа", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
         }
 
 
@@ -74,8 +81,31 @@ namespace WinFormsApp1
 
         private void button2_Click(object sender, EventArgs e) // Удаление заказа 
         {
-            listView1.Items.RemoveAt(int.Parse(textBox2.Text));
-            textBox2.Clear();
+            if (listView1.Items.Count > 0)
+            {
+                if (!string.IsNullOrWhiteSpace(textBox2.Text))
+                {
+                    if (listView1.Items.Count > int.Parse(textBox2.Text))
+                    {
+                        listView1.Items.RemoveAt(int.Parse(textBox2.Text));
+                        textBox2.Clear();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Введите существующий индекс заказа для удаления, отсчет начинается с 0", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        textBox2.Clear();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Заполните поле для удаления заказа", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Добавьте хотя бы один заказ для удаления", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textBox2.Clear();
+            }
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e) // Индекс заказа для замены
@@ -90,32 +120,64 @@ namespace WinFormsApp1
 
         private void button3_Click(object sender, EventArgs e) // Редактировать
         {
-            listView1.Items[int.Parse(textBox1.Text)].Text = textBox4.Text;
+            if (listView1.Items.Count > 0)
+            {
+                if ((!string.IsNullOrWhiteSpace(textBox1.Text)) && (!string.IsNullOrWhiteSpace(textBox4.Text)))
+                {
+                    if (listView1.Items.Count > int.Parse(textBox1.Text))
+                    {
+                        listView1.Items[int.Parse(textBox1.Text)].Text = textBox4.Text;
 
-            textBox1.Clear();
-            textBox4.Clear();
+                        textBox1.Clear();
+                        textBox4.Clear();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Введите существующий индекс заказа для редактирования, отсчет начинается с 0", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        textBox1.Clear();
+                        textBox4.Clear();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Заполните оба поля для редактирования", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    textBox1.Clear();
+                    textBox4.Clear();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Добавьте хотя бы один заказ для редактирования", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textBox1.Clear();
+                textBox4.Clear();
+            }
         }
 
         private void button4_Click(object sender, EventArgs e) // Оплатить заказ
         {
             int count = listView1.Items.Count;
-
-            for (int i = 0; i < count; i++)
+            if (count > 0)
             {
-                order = listView1.Items[i].Text;
-                ListViewItem item = new ListViewItem(order);
-                listView2.Items.Add(item);
-                string a = (number).ToString();
-                number++;
-                item.SubItems.Add(a);
-                item.SubItems.Add("оплачено");
+                for (int i = 0; i < count; i++)
+                {
+                    order = listView1.Items[i].Text;
+                    ListViewItem item = new ListViewItem(order);
+                    listView2.Items.Add(item);
+                    string a = (number).ToString();
+                    number++;
+                    item.SubItems.Add(a);
+                    item.SubItems.Add("оплачено");
+                }
+                sum = 0;
+                string sumtext = sum.ToString();
+                label2.Text = sumtext;
+
+                listView1.Items.Clear();
             }
-
-            sum = 0;
-            string sumtext = sum.ToString();
-            label2.Text = sumtext;
-
-            listView1.Items.Clear();
+            else
+            {
+                MessageBox.Show("Добавьте хотя бы один заказ", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
         }
 
         private void listView2_SelectedIndexChanged(object sender, EventArgs e)
