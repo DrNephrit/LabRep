125f8a136181b45646f1ee9272b0aecb0cf8e902
Nephrit <andrylite2006@gmail.com>
2024-04-29 16:18:55 +0300

    код на проверку тестировщику

--- WinFormsApp1/WinFormsApp1/Form1.cs
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -11,6 +11,10 @@ namespace WinFormsApp1
 {
     public partial class Form1 : Form
     {
+        string order = "";
+        int sum = 0;
+        int number = 1;
+
         public Form1()
         {
             InitializeComponent();
@@ -18,7 +22,28 @@ namespace WinFormsApp1
 
         private void button1_Click(object sender, EventArgs e) // Добавление заказа
         {
+            ListViewItem item = new ListViewItem(textBox3.Text);
 
+            listView1.Items.Add(item);
+            textBox3.Clear();
+
+            int id = listView1.Items.Count;
+
+            string order = id.ToString();
+
+            item.SubItems.Add(order);
+            item.SubItems.Add("в корзине");
+
+            if (listView1.Items.Count > 0)
+            {
+                sum += 200;
+            }
+
+            string sumtext = sum.ToString();
+            if (listView1.Items.Count > 0)
+                label2.Text = sumtext;
+
+            textBox3.Clear();
         }
 
 
@@ -49,7 +74,8 @@ namespace WinFormsApp1
 
         private void button2_Click(object sender, EventArgs e) // Удаление заказа 
         {
-
+            listView1.Items.RemoveAt(int.Parse(textBox2.Text));
+            textBox2.Clear();
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e) // Индекс заказа для замены
@@ -64,12 +90,32 @@ namespace WinFormsApp1
 
         private void button3_Click(object sender, EventArgs e) // Редактировать
         {
+            listView1.Items[int.Parse(textBox1.Text)].Text = textBox4.Text;
 
+            textBox1.Clear();
+            textBox4.Clear();
         }
 
         private void button4_Click(object sender, EventArgs e) // Оплатить заказ
         {
+            int count = listView1.Items.Count;
 
+            for (int i = 0; i < count; i++)
+            {
+                order = listView1.Items[i].Text;
+                ListViewItem item = new ListViewItem(order);
+                listView2.Items.Add(item);
+                string a = (number).ToString();
+                number++;
+                item.SubItems.Add(a);
+                item.SubItems.Add("оплачено");
+            }
+
+            sum = 0;
+            string sumtext = sum.ToString();
+            label2.Text = sumtext;
+
+            listView1.Items.Clear();
         }
 
         private void listView2_SelectedIndexChanged(object sender, EventArgs e)
--- WinFormsApp1/WinFormsApp1/obj/Debug/net6.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/WinFormsApp1/obj/Debug/net6.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edae5614fe37c49a0d773d8d0b3cffdc6606aae2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f30ee31e5d49ead4fd47170c2abd39fb6815d8f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
